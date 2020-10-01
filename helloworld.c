f7240b24dc1277bda91f254f94ab10329877d84a
V-Soni <40788535+V-Soni@users.noreply.github.com>
2020-10-01 17:01:08 +0530

    No you can take input from user and display output

--- helloworld.c
+++ helloworld.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+#include <conio.h>
 
 int main(void)
 {
+    char s;
     printf("Hello, World!\r\n");
-
+    printf("What is your name?\r\n");
+    scanf("%c",&s);
+    printf("Welcome ",s);
     return 0;
 }
